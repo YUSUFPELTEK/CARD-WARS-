ad4e2759821ccbc00af6c202442ecce6f584bf45
YUSUFPELTEK <yusufpeltek0@gmail.com>
2023-04-14 23:19:19 +0300

    Initial Commit

---
+++ L3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  L3
+//
+//  Created by YUSUF on 14.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ L3/L3App.swift
@@ -0,0 +1,17 @@
+//
+//  L3App.swift
+//  L3
+//
+//  Created by YUSUF on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct L3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
